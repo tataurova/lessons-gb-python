bb74fab8bb96fa9c65ffcc6a14222377d291af3c
Olga Tataurova <tataurovaoe@yandex.ru>
2019-05-16 17:55:34 +0300

    Исправлено 6 задание (создание файла реализовано программно)

--- client_server_app/lesson_1_homework/6.py
+++ client_server_app/lesson_1_homework/6.py
@@ -8,6 +8,15 @@ import locale
 def_coding = locale.getpreferredencoding()
 print(def_coding)
 
+f_n = open('test_file.txt', 'w', encoding='utf-8')
+f_n.write('сетевое программирование \n сокет \n декоратор')
+f_n.close()
+print(type(f_n))
+
+
 with open('test_file.txt', encoding='utf-8') as f_n:
     for el_str in f_n:
         print(el_str, end='')
+
+
+
